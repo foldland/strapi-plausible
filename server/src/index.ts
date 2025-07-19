af5e804e58ef5f060e910d436d273dcdfb1e7aa8
douwepausma <douwepausma@gmail.com>
2025-07-19 20:40:36 +0200

    cleanup

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,25 +9,13 @@
  * Plugin server methods
  */
 import config from './config';
-// import contentTypes from './content-types';
 import configController from './controllers/config';
-// import middlewares from './middlewares';
-// import policies from './policies';
 import routes from './routes';
-// import services from './services';
-import configRoutes from './routes/config';
 
 export default {
-  // register,
-  // bootstrap,
-  // destroy,
   config,
   controllers: {
     config: configController
   },
   routes
-  // services,
-  // contentTypes,
-  // policies,
-  // middlewares,
 };
